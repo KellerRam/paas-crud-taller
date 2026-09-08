dce4f77f662215948151b6c8ee8ae13e9d0831d8
Keller <kellerdavidramirez@gmail.com>
2026-09-08 08:21:59 -0600

    Actualizar contenido del repositorio

--- database.js
+++ database.js
@@ -1,11 +1,9 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const connectionString = process.env.DATABASE_URL || process.env.DATABASE_PRIVATE_URL || process.env.DATABASE_PUBLIC_URL;
-
 const pool = new Pool({
-  connectionString: connectionString,
-  ssl: connectionString && !connectionString.includes('localhost') ? { rejectUnauthorized: false } : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 const initDb = async () => {
